58396b054aaf1ca8c982b42c3132d673cdb20c3b
Bram Altunian <baaltunian@yahoo.com>
2019-07-07 23:17:28 -0400

    Remove extra whitespace

--- ambiance.rb
+++ ambiance.rb
@@ -1,4 +1,3 @@
-
 live_loop :piano do
   sample :ambi_piano
   play choose([:D3, :F3, :A5]), amp: 1
@@ -7,20 +6,14 @@ live_loop :piano do
   sleep 0.25
 end
 
-
 live_loop :buzz_buzz do
   sample :ambi_soft_buzz
   play choose([:C3, :E3, :G3]), amp: 1
   sleep 0.25
 end
 
-
-
-
 live_loop :haunt do
   sample :ambi_haunted_hum
   play choose([:C3, :E3, :G3]), amp: 0.25
   sleep 0.25
 end
-
-
